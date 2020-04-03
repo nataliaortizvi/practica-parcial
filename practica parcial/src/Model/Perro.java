424e85d37391d058406a794325ea755d44f3e2c1
natalia ortiz <nataliaortiz@MacBook-Air-de-natalia-3.local>
2020-04-02 20:11:38 -0500

    primer commit

---
+++ practica parcial/src/Model/Perro.java
@@ -0,0 +1,10 @@
+package Model;
+
+public class Perro {
+	
+	public Perro() {
+		
+	}
+	
+
+}
---
+++ practica parcial/src/View/Main.java
@@ -0,0 +1,12 @@
+package View;
+
+import processing.core.PApplet;
+
+public class Main extends PApplet {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
